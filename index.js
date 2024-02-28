6e51f7d8053682ea66ba8bac83c9f1c76ea6cd0c
Daniel Saka <danielhundessa@yahoo.com>
2024-02-28 13:24:52 -0500

    margins and dimensions

--- index.js
+++ index.js
@@ -1,30 +1,47 @@
-// select the svg conatiner first
-const svg = d3.select('svg');
+// select the svg container first
+const svg = d3.select('.canvas')
+  .append('svg')
+    .attr('width', 600)
+    .attr('height', 600);
+
+// create margins & dimensions
+const margin = {top: 20, right: 20, bottom: 100, left: 100};
+const graphWidth = 600 - margin.left - margin.right;
+const graphHeight = 600 - margin.top - margin.bottom;
+
+const graph = svg.append('g')
+  .attr('width', graphWidth)
+  .attr('height', graphHeight)
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
 d3.json('menu.json').then(data => {
 
   const y = d3.scaleLinear()
-    .domain([0, 1000])
+    .domain([0, d3.max(data, d => d.orders)])
     .range([0, 500]);
 
-  //console.log(y(600))
+  const x = d3.scaleBand()
+    .domain(data.map(item => item.name))
+    .range([0, 500])
+    .paddingInner(0.2)
+    .paddingOuter(0.2);
 
   // join the data to circs
-  const rects = svg.selectAll('rect')
+  const rects = graph.selectAll('rect')
     .data(data);
 
   // add attrs to circs already in the DOM
-  rects.attr('width', 50)
+  rects.attr('width', x.bandwidth)
     .attr("height", d => y(d.orders))
     .attr('fill', 'orange')
-    .attr('x', (d, i) => i * 70)
+    .attr('x', d => x(d.name));
 
   // append the enter selection to the DOM
   rects.enter()
     .append('rect')
-      .attr('width', 50)
+      .attr('width', x.bandwidth)
       .attr("height", d => y(d.orders))
       .attr('fill', 'orange')
-      .attr('x', (d, i) => i * 70)
+      .attr('x', (d) => x(d.name));
 
 });
