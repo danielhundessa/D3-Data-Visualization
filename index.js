e24026c6457a456d8e8f948f02dacffad3ffe4d2
Daniel Saka <danielhundessa@yahoo.com>
2024-02-28 14:37:02 -0500

    applied real time data update with firebase cloud

--- index.js
+++ index.js
@@ -18,30 +18,44 @@ const graph = svg.append('g')
 const xAxisGroup = graph.append('g')
   .attr('transform', `translate(0, ${graphHeight})`)
 
+xAxisGroup.selectAll('text')
+  .attr('fill', 'orange')
+  .attr('transform', 'rotate(-40)')
+  .attr('text-anchor', 'end');
+
 const yAxisGroup = graph.append('g');
 
-db.collection('dishes').get().then(res => {
-
-  var data = [];
-  res.docs.forEach(doc => {
-    data.push(doc.data());
-  });
-
-  const y = d3.scaleLinear()
-    .domain([0, d3.max(data, d => d.orders)])
+const y = d3.scaleLinear()
     .range([graphHeight, 0]);
 
-  const x = d3.scaleBand()
-    .domain(data.map(item => item.name))
-    .range([0, graphWidth])
-    .paddingInner(0.2)
-    .paddingOuter(0.2);
+const x = d3.scaleBand()
+  .range([0, graphWidth])
+  .paddingInner(0.2)
+  .paddingOuter(0.2);
+
+// create & call axes
+const xAxis = d3.axisBottom(x);
+const yAxis = d3.axisLeft(y)
+  .ticks(3)
+  .tickFormat(d => d + ' orders');
+
+// the update function
+const update = (data) => {
 
   // join the data to circs
   const rects = graph.selectAll('rect')
     .data(data);
 
-  // add attrs to circs already in the DOM
+  console.log(rects);
+
+  // remove unwanted rects
+  rects.exit().remove();
+
+  // update the domains
+  y.domain([0, d3.max(data, d => d.orders)]);
+  x.domain(data.map(item => item.name));
+
+  // add attrs to rects already in the DOM
   rects.attr('width', x.bandwidth)
     .attr("height", d => graphHeight - y(d.orders))
     .attr('fill', 'orange')
@@ -57,18 +71,36 @@ db.collection('dishes').get().then(res => {
       .attr('x', (d) => x(d.name))
       .attr('y', d => y(d.orders));
 
-  // create & call axesit
-  const xAxis = d3.axisBottom(x);
-  const yAxis = d3.axisLeft(y)
-    .ticks(3)
-    .tickFormat(d => d + ' orders');
-
   xAxisGroup.call(xAxis);
   yAxisGroup.call(yAxis);
 
-  xAxisGroup.selectAll('text')
-    .attr('fill', 'orange')
-    .attr('transform', 'rotate(-40)')
-    .attr('text-anchor', 'end')
+};
+
+var data = [];
+
+db.collection('dishes').onSnapshot(res => {
+  
+  res.docChanges().forEach(change => {
+
+    const doc = {...change.doc.data(), id: change.doc.id};
+
+    switch (change.type) {
+      case 'added':
+        data.push(doc);
+        break;
+      case 'modified':
+        const index = data.findIndex(item => item.id == doc.id);
+        data[index] = doc;
+        break;
+      case 'removed':
+        data = data.filter(item => item.id !== doc.id);
+        break;
+      default:
+        break;
+    }
+
+  });
+
+  update(data);
 
 });
