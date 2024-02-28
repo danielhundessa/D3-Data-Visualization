56d31effb27ee03ffe79ea94f8f89d777a0e1aca
Daniel Saka <danielhundessa@yahoo.com>
2024-02-28 12:35:46 -0500

    multiple data

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const data = [
-    {width: 200, height: 100, fill: 'purple'}
+    {width: 200, height: 100, fill: 'purple'},
+    {width: 100, height: 60, fill: 'pink'},
+    {width: 50, height: 30, fill: 'red'}
   ];
   
   // select the svg container first
   const svg = d3.select('svg');
   
-  svg.select('rect')
+  // console.log(d3.selectAll('rect').data(data))
+  svg.selectAll('rect') 
     .data(data)
-    .attr('width', (d,i,n) => d.width)
+    .attr('width', d => d.width)
     .attr('height', d => d.height)
     .attr('fill', d => d.fill);
   
