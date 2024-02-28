02b7e8330490b2300bad5e0d17a0f0c18bb5bc00
Daniel Saka <danielhundessa@yahoo.com>
2024-02-28 12:45:45 -0500

    data retrieve from external JSON file

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const data = [
-    {width: 200, height: 100, fill: 'purple'},
-    {width: 100, height: 60, fill: 'pink'},
-    {width: 50, height: 30, fill: 'red'}
-  ];
-  
-  // select the svg conatiner first
-  const svg = d3.select('svg');
-  
-  // join the data to rects
-  const rects = svg.selectAll('rect')
+// select the svg conatiner first
+const svg = d3.select('svg');
+
+d3.json('planets.json').then(data => {
+
+  // join the data to circs
+  const circs = svg.selectAll('circle')
     .data(data);
-  
-  // add attrs to rects already in the DOM
-  rects.attr('width', d => d.width)
-    .attr('height', d => d.height)
+
+  // add attrs to circs already in the DOM
+  circs.attr('cy', 200)
+    .attr('cx', d => d.distance)
+    .attr('r', d => d.radius)
     .attr('fill', d => d.fill);
-  
+
   // append the enter selection to the DOM
-  const added = rects.enter()
-    .append('rect')
-      .attr('width', d => d.width)
-      .attr('height', d => d.height)
+  circs.enter()
+    .append('circle')
+      .attr('cy', 200)
+      .attr('cx', d => d.distance)
+      .attr('r', d => d.radius)
       .attr('fill', d => d.fill);
+
+});
